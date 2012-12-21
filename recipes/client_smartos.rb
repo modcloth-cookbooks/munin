06f5287048a13340de3703a4f3864065274eb3b0
Stephen Lauck <stpehen.lauck@gmail.com>
2012-12-20 16:35:05 -0800

    better default list of checks

--- recipes/client_smartos.rb
+++ recipes/client_smartos.rb
@@ -1,5 +1,7 @@
-# df if_err_net0 if_net0 io_busy_ramdisk io_busy_sd io_bytes_ramdisk io_bytes_sd io_ops_ramdisk io_ops_sd iostat load memory netstat ntp_offset paging_in paging_out postfix_mailqueue sm_cpu smart_ uptime users vmstat
-checks_off = %w{ df_inode cpu if_err_net0 if_net0 io_busy_ramdisk io_busy_sd io_bytes_ramdisk io_bytes_sd io_ops_ramdisk io_ops_sd iostat netstat ntp_offset paging_in paging_out postfix_mailqueue users vmstat }
+# list of default checks for smartos
+# df_inode if_err_net0 if_net0 io_busy_ramdisk io_busy_sd io_bytes_ramdisk io_bytes_sd io_ops_ramdisk io_ops_sd iostat   netstat ntp_offset paging_in paging_out postfix_mailqueue   users vmstat
+
+checks_off = %w{ df_inode if_err_net0 if_net0 io_busy_ramdisk io_busy_sd io_bytes_ramdisk io_bytes_sd io_ops_ramdisk io_ops_sd iostat netstat ntp_offset paging_in paging_out postfix_mailqueue users vmstat }
 
 checks_off.each do |c|
 	munin_plugin c do
