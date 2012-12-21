a23366ce9c94c57e80bb95269d71744e88ef5e17
Stephen Lauck <stpehen.lauck@gmail.com>
2012-12-20 16:23:09 -0800

    only keep useful default checks for smartmachines

--- recipes/client_smartos.rb
+++ recipes/client_smartos.rb
@@ -1,9 +1,10 @@
-munin_plugin "cpu" do
-	enable false
-end
+# df if_err_net0 if_net0 io_busy_ramdisk io_busy_sd io_bytes_ramdisk io_bytes_sd io_ops_ramdisk io_ops_sd iostat load memory netstat ntp_offset paging_in paging_out postfix_mailqueue sm_cpu smart_ uptime users vmstat
+checks_off = %w{ df_inode cpu if_err_net0 if_net0 io_busy_ramdisk io_busy_sd io_bytes_ramdisk io_bytes_sd io_ops_ramdisk io_ops_sd iostat netstat ntp_offset paging_in paging_out postfix_mailqueue users vmstat }
 
-munin_plugin "df_inode" do
-  enable false
+checks_off.each do |c|
+	munin_plugin c do
+		enable false
+	end
 end
 
 munin_plugin "sm_memory" do
