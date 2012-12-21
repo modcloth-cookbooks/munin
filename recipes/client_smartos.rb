da5924ca82ae1ecdab31f6b537f497a3df85fb56
Stephen Lauck <stpehen.lauck@gmail.com>
2012-12-20 16:16:52 -0800

    removed un used checks and add smartos specific checks

--- recipes/client_smartos.rb
+++ recipes/client_smartos.rb
@@ -1,3 +1,11 @@
+munin_plugin "cpu" do
+	enable false
+end
+
+munin_plugin "df_inode" do
+  enable false
+end
+
 munin_plugin "sm_memory" do
   create_file true
   plugin "memory"
@@ -7,3 +15,4 @@ munin_plugin "sm_cpu" do
   create_file true
   plugin "sm_cpu"
 end
+
